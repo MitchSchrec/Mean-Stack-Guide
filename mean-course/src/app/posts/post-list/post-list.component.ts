a1cc9c938f5549acd7e64031bccafe87ee98bc1b
Mitchell Schrecongost <Mitchell@Schrecongost.com>
2019-02-02 20:09:33 -0500

    Basic Post Form Finished

--- mean-course/src/app/posts/post-list/post-list.component.ts
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -1,33 +1,35 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from '../posts.service';
 import {Post} from '../post.model';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
-  //   {
-  //     title: 'First Post',
-  //     content: 'This is the first post\'s content'
-  //   },
-  //   {
-  //     title: 'Second Post',
-  //     content: 'This is the second post\'s content'
-  //   },
-  //   {
-  //     title: 'Third Post',
-  //     content: 'This is the third post\'s content'
-  //   },
+  //   {title: 'First Post', content: 'This is the first post\'s content'},
+  //   {title: 'Second Post', content: 'This is the second post\'s content'},
+  //   {title: 'Third Post', content: 'This is the third post\'s content'},
   // ];
 
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
-  constructor() {
-  }
+  constructor(public postsService: PostsService) {}
 
   ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub =  this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe;
   }
 
 }
